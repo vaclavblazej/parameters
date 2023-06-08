f87de8d2bb9bc445fc276fecfa8b784e3731bc33
Václav Blažej <vaclav.blazej@warwick.ac.uk>
2023-06-08 23:37:11 +0200

    update build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-target_folder="$HOME/Dropbox/Projects/vasek_pages/content/material/science/cpx/parameters"
+cd "$(dirname "$0")" || exit
+target_folder="../static"
 ./draw.py
+mkdir -p ../build/
 cp "./page.md" "$target_folder/index.md"
 cp "./parameters.pdf" "$target_folder/parameters.pdf"
